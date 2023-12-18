fb4781bf0ae838137f38d8f9359b54a542d12c25
Syver <114483860+syver-syver@users.noreply.github.com>
2023-12-18 18:53:18 +0100

    a

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -1,7 +1,6 @@
 export async function GET() {
   return new Response(
-    `
-		<?xml version="1.0" encoding="UTF-8" ?>
+    `<?xml version="1.0" encoding="UTF-8" ?>
 		<urlset
 			xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
 			xmlns:xhtml="https://www.w3.org/1999/xhtml"
