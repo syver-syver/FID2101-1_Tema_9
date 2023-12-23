2cd84b88fd7455251c977e71ac85b2a8d8615c03
Syver <114483860+syver-syver@users.noreply.github.com>
2023-12-23 02:23:47 +0100

    a

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -1,45 +1,37 @@
 export async function GET() {
   return new Response(
-    `<?xml version="1.0" encoding="UTF-8" ?>
-		<urlset
-			xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
-			xmlns:xhtml="https://www.w3.org/1999/xhtml"
-			xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
-			xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
-			xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
-			xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
-		>
+    `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset
+          xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+                http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
     <url>
       <loc>https://syver-g.netlify.app/</loc>
-      <lastmod>2023-12-12</lastmod>
-      <changefreq>monthly</changefreq>
-      <priority>0.8</priority>
+      <lastmod>2023-12-23T01:19:47+00:00</lastmod>
+      <priority>1.00</priority>
     </url>
     <url>
-      <loc>https://syver-g.netlify.app/projects/tema_8/</loc>
-      <lastmod>2023-12-12</lastmod>
-      <changefreq>monthly</changefreq>
-      <priority>0.2</priority>
-    </url>
-    <url>
-      <loc>https://syver-g.netlify.app/projects/tema_5/</loc>
-      <lastmod>2023-12-12</lastmod>
-      <changefreq>monthly</changefreq>
-      <priority>0.2</priority>
-    </url>
-    <url>
-      <loc>https://syver-g.netlify.app/about/</loc>
-      <lastmod>2023-12-12</lastmod>
-      <changefreq>monthly</changefreq>
-      <priority>0.5</priority>
+      <loc>https://syver-g.netlify.app/about</loc>
+      <lastmod>2023-12-23T01:19:47+00:00</lastmod>
+      <priority>0.80</priority>
     </url>
     <url>
       <loc>https://syver-g.netlify.app/contact</loc>
-      <lastmod>2023-12-12</lastmod>
-      <changefreq>monthly</changefreq>
-      <priority>0.5</priority>
+      <lastmod>2023-12-23T01:19:47+00:00</lastmod>
+      <priority>0.80</priority>
     </url>
-		</urlset>`.trim(),
+    <url>
+      <loc>https://syver-g.netlify.app/projects/tema_8</loc>
+      <lastmod>2023-12-23T01:19:47+00:00</lastmod>
+      <priority>0.80</priority>
+    </url>
+    <url>
+      <loc>https://syver-g.netlify.app/projects/tema_5</loc>
+      <lastmod>2023-12-23T01:19:47+00:00</lastmod>
+      <priority>0.80</priority>
+    </url>
+    </urlset>`.trim(),
     {
       headers: {
         'Content-Type': 'application/xml'
