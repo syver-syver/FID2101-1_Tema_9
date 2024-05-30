5346acfcb21404bbae9b37f90223a4a1c85060e2
Syver <114483860+syver-syver@users.noreply.github.com>
2024-05-30 15:46:22 +0200

    a

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -1,11 +1,7 @@
 export async function GET() {
   return new Response(
     `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset
-          xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-          xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-                http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
       <url>
         <loc>https://syver-g.netlify.app/</loc>
         <lastmod>2024-05-30T13:50:26+02:00</lastmod>
