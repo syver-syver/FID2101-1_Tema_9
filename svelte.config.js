337f8f845c42926e182172dadc9c327d0f78eb64
Syver <114483860+syver-syver@users.noreply.github.com>
2024-06-23 21:11:42 +0200

    fixed adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,7 @@
 import adapter from '@sveltejs/adapter-vercel';
-
+ 
 export default {
-	kit: {
-		// default options are shown
-		adapter: adapter({
-			// if true, will create a Netlify Edge Function rather
-			// than using standard Node-based functions
-			edge: false,
-
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app.
-			// if `edge` is true, this option cannot be used
-			split: false
-		})
-	}
+  kit: {
+    adapter: adapter(),
+  },
 };
