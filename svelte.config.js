c141b9458a11ae0f2d212c7fba69ec501b9c1165
Syver <114483860+syver-syver@users.noreply.github.com>
2024-08-08 13:04:27 +0200

    a

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,11 @@
 import adapter from '@sveltejs/adapter-vercel';
- 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export default {
   kit: {
     adapter: adapter(),
-  },
+    // other configurations
+  }
 };
